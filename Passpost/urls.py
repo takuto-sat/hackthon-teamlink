7c4f1ef5040262ea738b1b4bb9273d0e6694eba3
Thyme0113 <77604820+Thyme0113@users.noreply.github.com>
2022-01-07 11:58:03 +0900

    登録画面のための変更：まだマージしないでね

--- Passpost/urls.py
+++ Passpost/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import signupview
+from .views import signupview, unlockview
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/',signupview,name='signup'),
+    path('unlock,', unlockview, name='unlock'),
 #    path('login/',loginview, name='login'),
 ]
--- Passpost/views.py
+++ Passpost/views.py
@@ -1,37 +1,39 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.models import User
-from django.db import IntegrityError
-from django.contrib.auth import authenticate,login
-
+from django.contrib.auth import authenticate, login as auth_login, get_user_model
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from .forms import SignUpForm, ProfileForm
+from .models import Profile
 
 # Create your views here.
+User = get_user_model()
+
+def unlockview(request):
+    return render(request, 'home.html')
+
 def signupview(request):
     if request.method == 'POST':
-        username_data = request.POST['username_data']
-        password_data = request.POST['password_data']
-        phone_number = request.POST.get('phone_number_data')
-        student_number = request.POST.get('student_number')
-        user = User.objects.create_user(username_data, '',password_data,'',phone_number,'',student_number)
-        try:
-            User.objects.create_user(username_data,'',password_data,'',phone_number,'',student_number)
-        except IntegrityError:
-            return render(request, 'signup.html',{'error':'このユーザーはすでに登録されています。'})
+        signup_form = SignUpForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+        if signup_form.is_valid() and profile_form.is_valid():
+            username = signup_form.cleaned_data.get('username')
+            email = signup_form.cleaned_data.get('phone_number')
+            password = signup_form.cleaned_data.get('password')
+            school_number = profile_form.cleaned_data.get('school_number')
+            user = User.objects.create_user(username, phone_number, password)
+            user.profile.school_number = school_number
+            user.save()
 
-
+            user = authenticate(request, username=username, password=password)
+            auth_login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            messages.add_message(request, messages.SUCCESS, 'ユーザー登録が完了しました！')
+            return redirect('unlock')
     else:
-        return render(request, 'signup.html', {})
-"abeyuki"
-"""
-def loginview(request):
-    if request.method=='POST':
-            username_data = request.POST['username_data']
-            password_data = request.POST['password_data']
-            user = authenticate(request, username=username_data,password=password_data)
-            if user is not None:
-                login(request,user)
-                return redirect('list')
-            else:
-                return redirect('login')
-    return render(request, 'login.html')
+        signup_form = SignUpForm()
+        profile_form = ProfileForm()
 
-"""
+    context = {
+        'signup_form': signup_form,
+        'profile_form': profile_form,
+    }
+    return render(request, 'signup.html', context)
