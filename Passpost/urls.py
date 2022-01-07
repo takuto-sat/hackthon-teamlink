a20af544ba940d306d5f9c3bc04e3a8355053a5e
Thyme0113 <77604820+Thyme0113@users.noreply.github.com>
2022-01-07 15:37:02 +0900

    ログイン画面完成

--- Passpost/urls.py
+++ Passpost/urls.py
@@ -4,6 +4,6 @@ from .views import signupview, unlockview
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/',signupview,name='signup'),
-    path('unlock,', unlockview, name='unlock'),
-#    path('login/',loginview, name='login'),
+    path('unlock', unlockview, name='unlock'),
+    path('login/',loginview, name='login'),
 ]
